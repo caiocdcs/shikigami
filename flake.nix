f38274033b8acce31c45ecc6d10ce31b17fc18fb
Caio Silva <caio@cdcs.dev>
2026-06-06 15:12:10 +0100

    fix: binary-only flake for Linux, no source build, no macOS

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
-      fenix,
       flake-utils,
       nixpkgs,
     }:
@@ -19,43 +14,37 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = fenix.packages.${system}.stable.toolchain;
+
+        version = "0.1.1";
+        arch =
+          {
+            x86_64-linux = "x86_64-linux";
+            aarch64-linux = "aarch64-linux";
+          }
+          .${system} or null;
+
+        sha256 =
+          {
+            x86_64-linux = "9d76e7d95059e47811961727b9df8791afc99a9de837467331f9240e9033f684";
+            aarch64-linux = "5e9a4c4efa91f94cc0542c3e8def017b6a8b4a3029bfdf889e5b09544fc760a0";
+          }
+          .${arch} or null;
+
       in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            toolchain
-            sqlx-cli
-            cargo-watch
-            just
-
-            # sqlite
-            sqlite
-          ];
-
-          SQLX_OFFLINE = "true";
-          DATABASE_URL = "sqlite:shikigami.db?mode=rwc";
-
-          shellHook = ''
-            echo "shikigami dev shell"
-          '';
-        };
-
+      pkgs.lib.optionalAttrs (arch != null) {
         packages.default = pkgs.stdenv.mkDerivation {
-          name = "shikigami";
-          src = ./.;
-          nativeBuildInputs = [
-            toolchain
-            pkgs.cacert
-          ];
-          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-          CARGO_HOME = "/tmp/cargo-home";
-          SQLX_OFFLINE = "true";
-          buildPhase = ''
-            cargo build --release
-          '';
+          pname = "shikigami";
+          inherit version;
+          src = pkgs.fetchurl {
+            url = "https://github.com/caiocdcs/shikigami/releases/download/v${version}/shikigami-${arch}.tar.gz";
+            inherit sha256;
+          };
+          sourceRoot = ".";
+          nativeBuildInputs = [ pkgs.patchelf ];
           installPhase = ''
-            install -Dm755 target/release/shikigami -t $out/bin
+            install -Dm755 shikigami -t $out/bin
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+              --set-rpath "${pkgs.stdenv.cc.cc.lib}/lib" $out/bin/shikigami
           '';
         };
       }
